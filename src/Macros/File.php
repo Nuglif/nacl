60b9b8ebd49a5d1e23d643ece3aeaa74b5f71a11
Pierrick Charron <pierrick@webstart.fr>
2019-10-29 23:01:51 -0400

    Add LICENSE

--- src/Macros/File.php
+++ src/Macros/File.php
@@ -1,4 +1,15 @@
 <?php
+/**
+ * This file is part of NACL.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @copyright 2019 Nuglif (2018) Inc.
+ * @license   http://www.opensource.org/licenses/mit-license.html  MIT License
+ * @author    Pierrick Charron <pierrick@adoy.net>
+ * @author    Charle Demers <charle.demers@gmail.com>
+ */
 
 namespace Nuglif\Nacl\Macros;
 
